5b6d3606b9a9ea6dc0285c88b1a0bc982bbe0b5d
Kenneth Ramírez Sánchez <desycorp@gmail.com>
2016-04-22 11:01:13 -0600

    Agregando archivos al repositorio

---
+++ src/test/java/HomeTests/TripAdvisorHomePageTest.java
@@ -0,0 +1,26 @@
+package HomeTests;
+
+import org.testng.annotations.Test;
+import pages.SeleniumDemoHomePage;
+import pages.TripAdvisorHomePage;
+import utility.TestBase;
+import utility.WebdriverUtils;
+
+/**
+ * Created by moises.siles on 4/19/2016.
+ */
+public class TripAdvisorHomePageTest extends TestBase {
+
+    @Test(description = "Selenium Demo Page", groups = {"factory"})
+    public void homePageTestLoad() throws Exception{
+
+        WebdriverUtils.goToURL("https://www.tripadvisor.com.mx");
+        WebdriverUtils.pauseTime(5000);
+
+        TripAdvisorHomePage tripAdvisorHomePage = new TripAdvisorHomePage(WebdriverUtils.driver);
+
+        tripAdvisorHomePage.printLinksText();
+
+        tripAdvisorHomePage.iniciaSesionClick();
+    }
+}
